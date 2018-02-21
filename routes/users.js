c43305e8f980b8c6b690c11e21099d6eae4751bc
mattNat <mattsanpedro76@gmail.com>
2018-02-20 19:43:04 -0800

    add validatePassword and hashPassword to /models/user.js

--- routes/users.js
+++ routes/users.js
@@ -14,34 +14,56 @@ router.post('/users', (req, res) => {
   let { fullname, username, password } = req.body;
   // console.log(fullname, username);
   
-  return User
-    .find({ username })
-    .count()
-    .then(count => {
-      if (count > 0) {
-        return Promise.reject({
-          code: 422,
-          reason: 'ValidationError',
-          message: 'Username already taken',
-          location: 'username'
-        });
-      }
-      return User.create({
-        fullname,
-        username,
-        password
-      });
-    })
-    .then(user => {
-      return res.status(201).json(user);
-    })
-    .catch(err => {
-      console.log(err);
-      if (err.reason === 'ValidationError') {
-        return res.status(err.code).json(err);
-      }
-      res.status(500).json({ code: 500, message: 'Internal server error'});
-    });
+  return User.hashPassword(password)
+  .then(digest => {
+    const newUser = {
+      username,
+      password: digest,
+      fullname
+    };
+    return User.create(newUser);
+  })
+  .then(result => {
+    return res.status(201).location(`/api/users/${result.id}`).json(result);
+  })
+  .catch(err => {
+    if (err.code === 11000) {
+      err = new Error('The username already exists');
+      err.status = 400;
+    }
+    next(err);
+  });
+  
+  // return User
+  //   .find({ username })
+  //   .count()
+  //   .then(count => {
+  //     if (count > 0) {
+  //       return Promise.reject({
+  //         code: 422,
+  //         reason: 'ValidationError',
+  //         message: 'Username already taken',
+  //         location: 'username'
+  //       });
+  //     }
+  //     return User.create({
+  //       fullname,
+  //       username,
+  //       password
+  //     });
+  //   })
+  //   .then(user => {
+  //     return res.status(201).json(user);
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //     if (err.reason === 'ValidationError') {
+  //       return res.status(err.code).json(err);
+  //     }
+  //     res.status(500).json({ code: 500, message: 'Internal server error'});
+  //   });
+
+
 });
 
 module.exports = router;
